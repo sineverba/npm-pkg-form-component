74b1f6ac0a3b1757062c3881931a2f734b6f6fd0
Alessandro Arici <alessandro.arici@gmail.com>
2025-11-05 11:30:25 +0100

    Upgrade dependencies

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -1,4 +1,7 @@
-# 1.4.0
+# Next version
++ Upgrade dependencies
+
+## 1.4.0
 + Upgrade dependencies
 + Add default value to number
 
--- src/__tests__/formComponent.test.tsx
+++ src/__tests__/formComponent.test.tsx
@@ -533,4 +533,16 @@ describe("FormComponent Tests", () => {
     expect(inputNumber).toBeInTheDocument();
     expect(inputNumber).toHaveValue(666);
   });
+
+  /**
+   * Test if the FormComponent handles key down events when onKeyDownRegex is not set.
+   */
+  it("should handle key down events without onKeyDown set", () => {
+    render(<FormComponent field={fields[0]} />);
+    const fooNameInputText = screen.getByLabelText(/fooName/i);
+    expect(fooNameInputText).toBeInTheDocument();
+
+    // Simulate key down event without regex validation
+    fireEvent.keyDown(fooNameInputText, { key: "a" });
+  });
 });
